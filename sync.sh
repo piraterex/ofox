ce2f106512d1432db1ee77d05a84380ef739baa4
ceb <henloboii@protonmail.ch>
2021-03-01 09:53:49 +0000

    add dummy sync script for TWRP

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source config/build*.sh
+
+repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
+
+repo sync -c -j48 --no-clone-bundle --no-tags 
