c7c15dffe93d68df335811f52ac2ca5ce7450a6d
ceb <henloboii@protonmail.ch>
2021-03-01 09:58:18 +0000

    Build.sh : initialize

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This, is the main point, where all things change
+
+# Clone tree START
+git clone https://github.com/JamieHoSzeYui/omni_device_xiaomi_merlin device/xiaomi/merlin # Clone your trees here
+# CLONE TREE END
+
+# VARIABLES, DEFINE THEM ELSE YOU'RE GAY
+DEVICE=merlin
+TARGET=recoveryimage
+# END VARIABLES
+
+. build/envsetup.sh
+lunch omni_$DEVICE-eng
+mka $TARGET -j48
+
+# Upload
+cd out/target/product/$DEVICE/
+touch links.txt
+echo "Recovery image: "
+transfer wet recovery.img  | grep Download >> links.txt
