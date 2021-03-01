3753a2718c4a33afef5f56436680086e365de19c
ceb <henloboii@protonmail.ch>
2021-03-01 10:02:02 +0000

    name fixup

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source config/build*.sh
+source configs/build*.sh
 
 repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 
